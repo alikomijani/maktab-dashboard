7850cb55da2da08778ab435bc801ffda71ac6455
Ali <a.komijani@wallex.net>
2023-11-24 10:11:21 +0330

    use new menu item component

--- src/components/base-components/layouts/dashboard/SidebarList.jsx
+++ src/components/base-components/layouts/dashboard/SidebarList.jsx
@@ -1,47 +1,13 @@
-import {
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { List } from "@mui/material";
 import React from "react";
 import { MenuItems } from "./constants";
-
+import MenuItem from "./MenuItem";
 function SidebarList() {
   return (
     <nav aria-label="dashboard menu">
       <List>
-        {MenuItems.map(({ Icon, ...item }) => (
-          <ListItem
-            disablePadding
-            key={item.id}
-            sx={{
-              borderRadius: "4px",
-              "& .MuiButtonBase-root": {
-                borderRadius: "4px",
-              },
-              "& .MuiButtonBase-root:hover": {
-                background: "#555555",
-              },
-            }}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <Icon
-                  sx={{
-                    color: (theme) => theme.palette.neutral[400],
-                  }}
-                />
-              </ListItemIcon>
-              <ListItemText
-                primary={item.label}
-                sx={{
-                  color: (theme) => theme.palette.neutral[300],
-                }}
-              />
-            </ListItemButton>
-          </ListItem>
+        {MenuItems.map((item) => (
+          <MenuItem item={item} key={item.id} />
         ))}
       </List>
     </nav>
